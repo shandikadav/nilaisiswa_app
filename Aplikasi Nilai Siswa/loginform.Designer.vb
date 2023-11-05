5513a58346ff73dae21f9f8ab34373036e6670e5
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2023-11-06 02:48:32 +0700

    first commit

---
+++ Aplikasi Nilai Siswa/loginform.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class loginform
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.txt_pass = New System.Windows.Forms.TextBox()
+        Me.txt_uname = New System.Windows.Forms.TextBox()
+        Me.btn_login = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Cambria", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label1.Location = New System.Drawing.Point(44, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(376, 32)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "APLIKASI SISTEM PENILAIAN"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(177, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(111, 41)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(250, 19)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "SMK MUHAMMADIYAH 3 METRO"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label4.Location = New System.Drawing.Point(109, 139)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(79, 16)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "PASSWORD"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label3.Location = New System.Drawing.Point(109, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 16)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "USERNAME"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.CheckBox1.Location = New System.Drawing.Point(112, 185)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(98, 17)
+        Me.CheckBox1.TabIndex = 13
+        Me.CheckBox1.Text = "Lihat Password"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'txt_pass
+        '
+        Me.txt_pass.Location = New System.Drawing.Point(112, 158)
+        Me.txt_pass.Name = "txt_pass"
+        Me.txt_pass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txt_pass.Size = New System.Drawing.Size(251, 20)
+        Me.txt_pass.TabIndex = 12
+        '
+        'txt_uname
+        '
+        Me.txt_uname.Location = New System.Drawing.Point(112, 104)
+        Me.txt_uname.Name = "txt_uname"
+        Me.txt_uname.Size = New System.Drawing.Size(251, 20)
+        Me.txt_uname.TabIndex = 11
+        '
+        'btn_login
+        '
+        Me.btn_login.BackColor = System.Drawing.Color.FromArgb(CType(CType(6, Byte), Integer), CType(CType(12, Byte), Integer), CType(CType(253, Byte), Integer))
+        Me.btn_login.FlatAppearance.BorderSize = 0
+        Me.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_login.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_login.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.btn_login.Location = New System.Drawing.Point(316, 220)
+        Me.btn_login.Name = "btn_login"
+        Me.btn_login.Size = New System.Drawing.Size(115, 31)
+        Me.btn_login.TabIndex = 16
+        Me.btn_login.Text = "LOGIN"
+        Me.btn_login.UseVisualStyleBackColor = False
+        '
+        'loginform
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(463, 284)
+        Me.Controls.Add(Me.btn_login)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.txt_pass)
+        Me.Controls.Add(Me.txt_uname)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "loginform"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SMKBISA!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents txt_pass As TextBox
+    Friend WithEvents txt_uname As TextBox
+    Friend WithEvents btn_login As Button
+End Class
---
+++ Aplikasi Nilai Siswa/loginform.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.SqlClient
+Public Class loginform
+
+    Private Sub btn_login_Click(sender As Object, e As EventArgs) Handles btn_login.Click
+        konekdb()
+        Dim cmd As New SqlCommand("SELECT * FROM Akun WHERE Username = '" + txt_uname.Text + "' AND Password = '" + txt_pass.Text + "'", konek)
+        Dim rd As SqlDataReader
+        rd = cmd.ExecuteReader
+        rd.Read()
+        If rd.HasRows = True Then
+            If rd.Item(2).ToString = "Administrator" Then
+                MessageBox.Show("Login Administrator Berhasil!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                AdminMenu.Show()
+                Me.Hide()
+            ElseIf rd.Item(2).ToString = "Siswa" Then
+                MessageBox.Show("Login Siswa Berhasil!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                SiswaMenu.Show()
+                Me.Hide()
+            End If
+        End If
+
+    End Sub
+End Class
